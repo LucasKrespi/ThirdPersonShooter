0f93cdc5e9f3b0802165308e690ec1da32f825f7
lucaskrespi <lucas.krespi@gmail.com>
2023-09-28 15:56:02 -0700

    Initial commit

---
+++ Source/ThirdPersonShooter/ShooterCharacter.h
@@ -0,0 +1,49 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "ShooterCharacter.generated.h"
+
+UCLASS()
+class THIRDPERSONSHOOTER_API AShooterCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AShooterCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	void MoveForward(float Valeu);
+	void MoveRight(float Valeu);
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+private:
+	//Camera Boom Positioning the Camera behind the Character
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	class USpringArmComponent* CameraBoom;
+
+	//Camera that follows the character
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	class UCameraComponent* FollowCamera;
+
+public:
+
+	//Return Camera Boom subObject
+	FORCEINLINE USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
+	
+	//Return Camera Folloew subobject
+	FORCEINLINE UCameraComponent* GetFollowCamera() const { return FollowCamera; }
+
+};
